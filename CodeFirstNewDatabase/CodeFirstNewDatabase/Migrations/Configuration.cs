1567e13c73660821e121cec67eb5f451ebda2044
Grewalkirat007 <55209423+grewalkirat007@users.noreply.github.com>
2022-10-05 11:58:25 -0700

    Add files via upload

---
+++ CodeFirstNewDatabase/CodeFirstNewDatabase/Migrations/Configuration.cs
@@ -0,0 +1,24 @@
+﻿namespace CodeFirstNewDatabase.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<CodeFirstNewDatabase.BlogContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+            ContextKey = "CodeFirstNewDatabase.BlogContext";
+        }
+
+        protected override void Seed(CodeFirstNewDatabase.BlogContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
