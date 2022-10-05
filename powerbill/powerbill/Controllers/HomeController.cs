6c2185c7cd8b5cfae03297c6bf26636705ce86c2
Grewalkirat007 <55209423+grewalkirat007@users.noreply.github.com>
2022-10-05 13:20:32 -0700

    Add files via upload

---
+++ powerbill/powerbill/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using powerbill.Models;
+
+namespace powerbill.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            ViewBag.ans = 0;
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(bill model)
+        {
+            if (ModelState.IsValid)
+            {
+                ViewBag.ans = model.main();
+            }
+            else
+            {
+                ViewBag.ans = 0;
+            }
+            return View(model);
+        }
+    }
+}
---
+++ powerbill/powerbill/Models/bill.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace powerbill.Models
+{
+    public class bill
+    {
+        [Required(ErrorMessage = "Enter Num")]
+        public decimal? total_kilowatt { get; set; }
+        [Required(ErrorMessage = "Enter Num")]
+        public decimal? hours { get; set; }
+        [Required(ErrorMessage = "Enter Num")]
+        public decimal? cost { get; set; }
+        [Required(ErrorMessage = "Enter Num")]
+        public decimal? ans { get; set; }
+
+        public decimal? main()
+        {
+            ans = total_kilowatt * ( hours * 30 ) * cost;
+            return ans;
+        }
+    }
+}
