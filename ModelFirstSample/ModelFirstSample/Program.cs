22cc6546ae111b04151d088bd0b117f245718f82
Grewalkirat007 <55209423+grewalkirat007@users.noreply.github.com>
2022-10-05 13:19:13 -0700

    Add files via upload

---
+++ ModelFirstSample/ModelFirstSample/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModelFirstSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var db = new BloggingContext())
+            {
+                Console.Write("Enter a name for a new blog: ");
+                var name = Console.ReadLine();
+
+                var blog = new Blog { Name = name };
+                db.Blogs.Add(blog);
+                db.SaveChanges();
+
+                var query = from b in db.Blogs
+                            orderby b.Name
+                            select b;
+
+                foreach (var item in query)
+                {
+                    Console.WriteLine(item.Name);
+                }
+            }
+        }
+    }
+}
